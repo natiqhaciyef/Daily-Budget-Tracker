b4fbfac8a50373d0255be16d1d6045141ce518c1
Natiq <natiq00h2272@gmail.com>
2023-01-30 20:44:28 +0400

    Adapter connected with recyclerview

--- app/src/main/java/com/natiqhaciyef/dailybudgettracker/presentation/view/HomeFragment.kt
+++ app/src/main/java/com/natiqhaciyef/dailybudgettracker/presentation/view/HomeFragment.kt
@@ -10,8 +10,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.natiqhaciyef.dailybudgettracker.R
 import com.natiqhaciyef.dailybudgettracker.databinding.FragmentHomeBinding
+import com.natiqhaciyef.dailybudgettracker.presentation.adapter.ExpenseCategoryAdapter
 import com.natiqhaciyef.dailybudgettracker.presentation.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -20,10 +22,13 @@ import org.eazegraph.lib.models.PieModel
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var adapter: ExpenseCategoryAdapter
     private val viewModel: HomeViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         return binding.root
     }
@@ -38,9 +43,19 @@ class HomeFragment : Fragment() {
         binding.pieChart.startAnimation()
     }
 
-    fun observeLiveData(){
-        viewModel.liveBudgetModel.observe(viewLifecycleOwner){
-            Log.e("LLL","${it.data}")
+    private fun observeLiveData() {
+        viewModel.liveBudgetModel.observe(viewLifecycleOwner) {
+            it.data?.let { budgetModel ->
+                adapter =
+                    ExpenseCategoryAdapter(
+                        requireContext(),
+                        mutableListOf(),
+                        budgetModel.totalPrice
+                    )
+                binding.categoryRecyclerView.layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+                binding.categoryRecyclerView.adapter = adapter
+            }
         }
     }
 }
